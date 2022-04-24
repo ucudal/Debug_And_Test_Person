6f0bf11612f74b41aed2bc79e9ef92a1eeacd5b7
Juan Riccetto <juanmartinre@gmail.com>
2022-04-24 20:49:14 -0300

    Tests realizados correctamente

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -9,13 +9,72 @@ namespace Tests
         [SetUp]
         public void Setup()
         {
-            // Insertá tu código de inicialización aquí
+
         }
 
+        /// <summary>
+        /// Test para cuando el nombre es inválido, ya que devuelve basura.
+        /// </summary>
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void InvalidNameTest() // Cambiá el nombre para indicar qué estás probando
         {
-            // Insertá tu código  de pruebaaquí
+            Person emptyName = new Person("", "5324939-0");
+            Assert.IsNull(emptyName.Name);
+            Assert.IsNotNull(emptyName.ID);
+        }
+
+        /// <summary>
+        /// Test para cuando la cédula es inválida.
+        /// </summary>
+        [Test]
+        public void InvalidIDTest()
+        {
+            Person emptyCI = new Person("John Doe", "");
+            Assert.IsNull(emptyCI.ID);
+            Assert.IsNotNull(emptyCI.Name);
+        }
+
+        /// <summary>
+        /// Test para cuando el nombre es válido.
+        /// </summary>
+        [Test]
+        public void ValidNameTest()
+        {
+            Person validName = new Person("John Doe", "");
+            Assert.IsNotEmpty(validName.Name);
+            Assert.IsNull(validName.ID);
+        }
+
+        /// <summary>
+        /// Test para cuando la cédula es válida
+        /// </summary>
+        [Test]
+        public void ValidIDTest()
+        {
+            Person validID = new Person("", "5324939-0");
+            Assert.IsNotNull(validID.ID);
+            Assert.IsNull(validID.Name);
+        }
+
+        /// <summary>
+        /// Test para cuando ambos parámetros son válidos.
+        /// </summary>
+        [Test]
+        public void ValidPersonTest()
+        {
+            Person valid = new Person("John Doe", "5324939-0");
+            Assert.IsInstanceOf<Person>(valid);
+        }
+        
+        /// <summary>
+        /// Test para cuando ambos parámetros son validos.
+        /// </summary>
+        [Test]
+        public void InValidPersonTest()
+        {
+            Person invalid = new Person("", "5324939");
+            Assert.IsNull(invalid.Name);
+            Assert.IsNull(invalid.ID);
         }
     }
 }
